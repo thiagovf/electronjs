0d9207c1dffc117efc930986a5db992436c4e1a7
Thiago Fernandes <thiagofernandes@Thiago.local>
2023-03-06 10:05:15 -0300

    Initial changes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules
 const {app, BrowserWindow} = require('electron')
+const colors = require('colors')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
